6b449bc99861e8242d94e545ab327aab5ac2b6b8
Aziz Limonu <azizlimonu@gmail.com>
2022-11-22 16:55:28 +0800

    create async thunk

--- client/src/features/reducer/productSlice.js
+++ client/src/features/reducer/productSlice.js
@@ -1,6 +1,42 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from 'axios';
+import { url } from "./apiSlice";
 
-const initialState={
-  items:[],
-  status:null,
-  createStatus:null,
+const initialState = {
+  items: [],
+  status: null,
+  createStatus: null,
 };
+
+export const fetchProduct = createAsyncThunk(
+  "products/fetchProduct", async () => {
+    try {
+      const response = await axios.get(`${url}/products`);
+      return response.data;
+    } catch (error) {
+      console.log("Error fetch product",error);
+    }
+  }
+);
+
+export const createProduct = createAsyncThunk();
+
+const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers:{
+    [fetchProduct.pending]:(state,action)=>{
+      state.status="pending";
+    },
+    [fetchProduct.fulfilled]:(state,action)=>{
+      state.items=action.payload;
+      state.status="success";
+    },
+    [fetchProduct.rejected]:(state,action)=>{
+      state.status="rejected";
+    }
+  }
+});
+
+export default productSlice.reducer;
--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { store } from './features/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
