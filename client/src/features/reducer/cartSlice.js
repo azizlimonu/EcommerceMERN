0ca52952221e9338de983128acf77c6b7adfac43
Aziz Limonu <azizlimonu@gmail.com>
2022-11-22 16:13:13 +0800

    adding reducer for cart logic

--- client/src/features/reducer/cartSlice.js
+++ client/src/features/reducer/cartSlice.js
@@ -28,9 +28,91 @@ const cartSlice = createSlice({
       };
       // set to the local storage
       localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+    decreaseCart(state, action) {
+      const itemIndex = state.cartItems.findIndex(
+        (item) => item._id === action.payload._id
+      );
+      // if theres quantity >1 we decrease 1, if quantity 1 we erase
+      if (state.cartItems[itemIndex].cartQuantity > 1) {
+        state.cartItems[itemIndex].cartQuantity -= 1;
+      } else {
+        // we erase using filter method
+        const nextCartItems = state.cartItems.filter(
+          (item) => item._id !== action.payload._id
+        );
+        state.cartItems = nextCartItems;
+      }
+      // set to the local storage
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+    removeFromCart(state, action) {
+      state.cartItems.map((cart) => {
+        if (cart._id === action.payload.id) {
+          const nextCartItems = state.cartItems.filter(
+            (item) => item._id !== cart._id
+          );
+          state.cartItems = nextCartItems;
+        }
+        localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+        return state;
+      });
+    },
+    clearCart(state, action) {
+      state.cartItems = [];
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+    increaseItemQty(state, action) {
+      const itemIndex = state.cartItems.findIndex(
+        (item) => item._id === action.payload._id
+      );
+      if (itemIndex >= 0) {
+        state.cartItems[itemIndex].cartQuantity += 1;
+      }
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+
+    },
+    decreaseItemQty(state, action) {
+      const itemIndex = state.cartItems.findIndex(
+        (item) => item._id === action.payload._id
+      );
+      if (state.cartItems[itemIndex].cartQuantity > 1) {
+        state.cartItems[itemIndex].cartQuantity -= 1;
+      }
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+
+    },
+    getTotals(state, action) {
+      let { total, quantity } = state.cartItems.reduce(
+        (cartTotal, cartItem) => {
+          const { price, cartQuantity } = cartItem;
+          const itemTotal = price * cartQuantity;
+
+          cartTotal.total += itemTotal;
+          cartTotal.quantity += cartQuantity;
+
+          return cartTotal;
+        },
+        {
+          total: 0,
+          quantity: 0,
+        }
+      );
+      total = parseFloat(total.toFixed(2));
+      state.cartTotalQuantity = quantity;
+      state.cartTotalAmount = total;
     }
   }
 });
 
-export const { addToCart } = cartSlice.actions;
+export const {
+  addToCart,
+  decreaseCart,
+  removeFromCart,
+  clearCart,
+  increaseItemQty,
+  decreaseItemQty,
+  getTotals
+} = cartSlice.actions;
+
 export default cartSlice.reducer;
