4fa7ffc040962ce13c74ee75ca8630a817192d26
Aziz Limonu <azizlimonu@gmail.com>
2022-11-25 14:10:39 +0800

    fix stripe payment and routes

--- backend/routes/stripeRoutes.js
+++ backend/routes/stripeRoutes.js
@@ -1,25 +1,91 @@
 const Stripe = require('stripe');
 const Order = require('../models/orderModel');
-
+const express = require('express');
 require('dotenv').config();
 const stripe = Stripe(process.env.STRIPE_KEY);
-const router = require('express').Router();
+const router = express.Router();
 
 // endpoint stripe
 router.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: 'T-shirt',
+  const customer=await stripe.customers.create({
+    metadata: {
+      userId: req.body.userId,
+      cart: JSON.stringify(req.body.cartItems),
+    },
+  });
+
+  const line_items = req.body.cartItems.map((item) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+          images: [item.image],
+          description: item.desc,
+          metadata: {
+            id: item.id,
           },
-          unit_amount: 2000,
         },
-        quantity: 1,
+        unit_amount: item.price * 100,
+      },
+      quantity: item.cartQuantity,
+    }
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    shipping_address_collection: {
+      allowed_countries: ["US", "CA"],
+    },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 0,
+            currency: "usd",
+          },
+          display_name: "Free shipping",
+          // Delivers between 5-7 business days
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 5,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 7,
+            },
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 1500,
+            currency: "usd",
+          },
+          display_name: "Next day air",
+          // Delivers in exactly 1 business day
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 1,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 1,
+            },
+          },
+        },
       },
     ],
+    phone_number_collection: {
+      enabled: true,
+    },
+    line_items,
+    customer: customer.id,
     mode: 'payment',
     success_url: `${process.env.CLIENT_URL}/checkout-success`,
     cancel_url: `${process.env.CLIENT_URL}/cart`,
@@ -28,7 +94,60 @@ router.post('/create-checkout-session', async (req, res) => {
   res.json({ url: session.url });
 });
 
+// stripe webhook
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+let endpointSecret;
+// endpointSecret = process.env.STRIPE_WEB_HOOK;
+
+router.post('/webhook', express.json({ type: 'application/json' }),
+  async (req, res) => {
+    let data;
+    let eventType;
+    
+    if (endpointSecret) {
+      const sig = req.headers['stripe-signature'];
+      let event;
+      try {
+        event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
+        console.log("webhook verified");
+      } catch (err) {
+        res.status(400).send(`Webhook Error: ${err.message}`);
+        console.log(`weebhook error ${err.message}`);
+        return;
+      }
+      data = event.data.object;
+      eventType = event.type;
+    } else {
+      data = req.body.data.object;
+      eventType = req.body.type
+    }
+
+    // handle the event
+    if (eventType === "checkout.session.completed") {
+      // get data payment & data customer
+      stripe.customers
+        .retrieve(data.customer)
+        .then((customer) => {
+          try {
+            // CREATE ORDER
+            // createOrder(customer, data);
+            console.log(customer)
+            // console.log("data",data);
+          } catch (err) {
+            // console.log(typeof createOrder);
+            console.log(err);
+          }
+        })
+        .catch((err) => console.log(err.message));
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    res.send().end();
+  });
+
 module.exports = router;
+
 
 // router.post('/create-checkout-session', async (req, res) => {
 //   const customer = await stripe.customers.create({
